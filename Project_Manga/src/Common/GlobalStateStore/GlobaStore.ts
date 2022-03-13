b6cde10db8fee214a71e4efa42bd7cd01138212c
Andries-Omega <achimule@jhb.dvt.co.za>
2022-03-13 17:03:06 +0200

    Made mouse side nav use redux toolkit instead of useReducer

--- Project_Manga/src/Common/GlobalStateStore/GlobaStore.ts
+++ Project_Manga/src/Common/GlobalStateStore/GlobaStore.ts
@@ -3,11 +3,15 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 interface GlobalStore {
   showFooter: boolean;
   darkMode: boolean;
+  sideHeaderMouseHover: boolean;
+  currentPage: string;
 }
 
 const initialState: GlobalStore = {
   showFooter: false,
-  darkMode: true,
+  darkMode: false,
+  sideHeaderMouseHover: false,
+  currentPage: "/",
 };
 
 export const globalSlice = createSlice({
@@ -20,9 +24,16 @@ export const globalSlice = createSlice({
     setFooter: (state, action: PayloadAction<boolean>) => {
       state.showFooter = action.payload;
     },
+    setSideMouseHover: (state, action: PayloadAction<boolean>) => {
+      state.sideHeaderMouseHover = action.payload;
+    },
+    setCurrentPage: (state, action: PayloadAction<string>) => {
+      state.currentPage = action.payload;
+    },
   },
 });
 
-export const { setDarkMode, setFooter } = globalSlice.actions;
+export const { setDarkMode, setFooter, setCurrentPage, setSideMouseHover } =
+  globalSlice.actions;
 
 export default globalSlice.reducer;
--- Project_Manga/src/Common/Header/SideRouting/SideRouting.tsx
+++ Project_Manga/src/Common/Header/SideRouting/SideRouting.tsx
@@ -1,71 +1,39 @@
-import { useReducer } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../store";
+import { setSideMouseHover } from "../../GlobalStateStore/GlobaStore";
 
-interface IViewState {
-  mouseEntered: boolean;
-  currentPage: string;
-}
-
-const viewState: IViewState = {
-  mouseEntered: false,
-  currentPage: "/",
-};
-
-const reducer = (
-  state: IViewState,
-  action: {
-    type: string;
-    payload?: any;
-  }
-) => {
-  switch (action.type) {
-    case "mouseEntered":
-      return {
-        ...state,
-        mouseEntered: action.payload,
-      };
-    case "mouseLeft":
-      return {
-        ...state,
-        mouseEntered: action.payload,
-      };
-    case "navigate":
-      return {
-        ...state,
-        currentPage: action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
 export default function SideRouting() {
-  const [state, dispatch] = useReducer(reducer, viewState);
   const darkMode = useSelector(
     (state: RootState) => state.globalState.darkMode
   );
+  const mouseHover = useSelector(
+    (state: RootState) => state.globalState.sideHeaderMouseHover
+  );
+  const currentPage = useSelector(
+    (state: RootState) => state.globalState.currentPage
+  );
+  const dispatch = useDispatch();
 
   let bgColor = darkMode ? "bg-gray-800" : "bg-slate-200"; // For BG Color to respond to darkmode or light mode
   return (
     <div
       className={
-        state.mouseEntered
+        mouseHover
           ? `${bgColor} w-52 float-left h-screen pt-5 ease-in duration-500 rounded-br-lg opacity-[.94]`
           : `${bgColor} w-16 float-left h-screen pt-5 ease-out duration-1000 rounded-br-lg`
       }
       onMouseEnter={() => {
-        dispatch({ type: "mouseEntered", payload: true });
+        dispatch(setSideMouseHover(true));
       }}
       onMouseLeave={() => {
-        dispatch({ type: "mouseLeft", payload: false });
+        dispatch(setSideMouseHover(false));
       }}
     >
       <div className="grid place-items-center ">
         <div>
           <a
             className={
-              state.currentPage === "/"
+              currentPage === "/"
                 ? " text-blue-500 text-xl cursor-pointer hover:text-blue-700 font-mono"
                 : "text-black text-xl cursor-pointer hover:text-blue-500 font-mono"
             }
@@ -74,7 +42,7 @@ export default function SideRouting() {
               <i className="fa-solid fa-house "></i>
             </span>
 
-            <span className={state.mouseEntered ? "" : "hidden"}>Home</span>
+            <span className={mouseHover ? "" : "hidden"}>Home</span>
           </a>
         </div>
       </div>
